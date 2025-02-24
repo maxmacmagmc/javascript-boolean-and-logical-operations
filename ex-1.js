0bf9fd646f1fd2ab1a7973929ee523dd7de72f92
Wutt Saeng <wuttforwork@gmail.com>
2025-02-24 15:31:51 +0700

    ADD:Boolean ex1-3

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,13 @@
+// Exercise #1: Event Conditions
+
 let isOver18;
 let hasCriminalBlacklist;
+let isThai;
 
 let isAllow;
 
 // Start coding here
-
-console.log(isAllow);
+isOver18 = false
+hasCriminalBlacklist = hasCriminalBlacklist == false
+isAllow = isOver18 && !hasCriminalBlacklist
+console.log (isAllow)
--- ex-2.js
+++ ex-2.js
@@ -9,4 +9,12 @@ let isPlatinum;
 
 let hasPromotion;
 
+
+lastMonthPaidMoreThan4000 = true
+isWeekday = true
+hasBoughtProductFromITCategory = true
+hasAttendedDiscountEvent = false
+isPlatinum = false
+
+hasPromotion = ((lastMonthPaidMoreThan4000 && isWeekday)&& (hasAttendedDiscountEvent && hasBoughtProductFromITCategory)) || isPlatinum
 console.log(hasPromotion);
